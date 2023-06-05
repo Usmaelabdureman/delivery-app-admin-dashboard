7c3a662834e8744e750fc562eda44ccd32afd19c
Usmaelabdureman <uabdureman@gmail.com>
2023-06-05 15:16:14 +0300

    update card

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 
-const Card = ({icon,desc,...rest}) => {
-  return (
-    <div className="bg-white rounded-lg p-6 shadow-md">
-      <h2 className="text-xl font-bold mb-4">{icon}</h2>
-      <div className="flex items-center">
-        <span className="text-4xl font-bold">${desc}</span>
-        <span className="ml-2 text-green-500">{...props} since yesterday</span>
+const Card = ({ icon, desc, percentage }) => {
+    return (
+      <div className="bg-white rounded-lg p-6 shadow-md">
+        <h2 className="text-xl font-bold mb-4">{icon}</h2>
+        <div className="flex items-center">
+          <span className="text-4xl font-bold">${desc}</span>
+          <span className="ml-2 text-green-500">{percentage}% since yesterday</span>
+        </div>
       </div>
-    </div>
-  );
-};
-
-export default Card;
+    );
+  };
+  
+  export default Card;
+  
